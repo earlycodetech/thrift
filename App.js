6c1a989c0ddbbff49e1dc38547e151f2a27e79fd
Joseph Ogbu <fallyfox@gmail.com>
2022-12-09 16:31:59 +0100

    signup screen des

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { Intro } from './screens/Intro';
-import { Home } from './screens/Home';
+import { Signup } from "./screens/Signup";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Signup />
     </>
   );
 }
